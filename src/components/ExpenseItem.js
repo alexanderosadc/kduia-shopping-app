3f0f23caf7e60031ced09e57edfe0ca7fce33ac7
alexanderosadc <alexanderosadc@gmail.com>
2023-08-15 16:37:19 +0300

    feat:  adds new elements with hooks and redux

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,25 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
-
 const ExpenseItem = (props) => {
+    const { dispatch, Location} = useContext(AppContext);
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.name,
+        };
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.quantity}</td>
+        <td>{Location}{parseInt(props.unitprice)}</td>
+        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td>
+        <td><button onClick={handleDeleteItem}><FaTimesCircle size='2.2em' color="red"></FaTimesCircle></button></td>
+        </tr>
+    );
 };
-
 export default ExpenseItem;
